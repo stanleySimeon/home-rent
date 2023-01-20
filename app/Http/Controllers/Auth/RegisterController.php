159cad02b775a92419af4b66dd74c11bf7642cde
mstanley.me@gmail.com <mstanley.me@gmail.com>
2023-01-19 22:32:12 -0500

    Add change

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,56 +5,51 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
-use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
-
-    use RegistersUsers;
-
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
     protected $redirectTo = RouteServiceProvider::HOME;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest');
     }
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
+    protected function validator(Request $request)
     {
-        return Validator::make($data, [
-            'avatar' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        // return Validator::make($data, [
+        //     'avatar' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:5120',
+        //     'name' => ['required', 'string', 'max:255'],
+        //     'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+        //     'password' => ['required', 'string', 'min:8', 'confirmed'],
+        //     'password_confirmation' => 'required|same:password',
+        // ]);
+
+        // $requestData = $data->except(['_token', 'password', 'password_confirmation']);
+        // $avatarName = 'lv_' . rand() . '.' . $data->avatar->extension();
+        // $data->avatar->move(public_path('storage/'), $avatarName);
+        // $requestData['avatar'] = $avatarName;
+
+        return Validator::make($request, [
+            'avatar' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:5120',
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password_confirmation' => 'required|same:password',
         ]);
+
+        // let request the data from the form and store it in the database
+        $requestData = $request->except(['_token', 'password', 'password_confirmation']);
+        $avatarName = 'lv_' . rand() . '.' . $request->avatar->extension();
+        $request->avatar->move(public_path('storage/'), $avatarName);
+        $requestData['avatar'] = $avatarName;
+        $requestData['name'] = $request->name;
+        $requestData['email'] = $request->email;
+        $requestData['password'] = Hash::make($request->password);
+        $requestData['password_confirmation'] = Hash::make($request->password_confirmation);
     }
 
 
@@ -64,16 +59,20 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \App\Models\User
      */
-    protected function create(array $data)
-    {
-        return User::create([
-            'avatar' => $data['avatar'],
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
-    }
+    // protected function create(array $data)
+    // {
+    //     return User::create([
+    //         'name' => $data['name'],
+    //         'email' => $data['email'],
+    //         'password' => Hash::make($data['password']),
+    //     ]);
+    // }
 
+    protected function create(Request $request)
+    {
+        $this->validator($request);
+        return redirect()->route('login');
+    }
     /**
      * Get the guard to be used during registration.
      *
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
@@ -17,8 +16,8 @@
 
 <body class="bg-[#fdfdfd]">
     <div class="flex h-screen">
-        <div class="container w-3/12 h-full bg-gradient-to-b from-[#021369] to-[#01122b]">
-            <ul class="flex justify-between items-center text-white">
+        <div class="container px-0 w-4/12 h-full space-y-4 bg-gradient-to-b from-[#021369] to-[#01122b] relative">
+            <ul class="w-full px-8 flex justify-between items-center text-white border-b border-gray-500 pb-4">
                 @guest
                 @if (Route::has('login'))
                 <li class="nav-item">
@@ -35,33 +34,41 @@
                 <li class="w-full flex justify-between items-center mt-4">
                     <!-- show user auth avatar -->
                     <span class="w-full flex space-x-8 items-center">
-                        <div class="flex space-x-2">
-                            <div class="bg-[#373737] rounded-full relative">
-                                <img src="{{ Auth::user()->avatar }}" alt="avatar" class="w-14 h-14 rounded-full">
-                                <span class="w-4 h-4 absolute left-8 right-0 ml-auto mr-auto -top-0 text-9xl z-10 bg-green-600 rounded-full">
-                                </span>
-                            </div>
+                        <div class="flex space-x-4">
+                            <span class="bg-[#373737] rounded-full">
+                                <!-- get the user avatar -->
+                                <img src="{{ URL::asset('/storage/'.Auth::user()->avatar) }}" alt="avatar" class="w-12 h-12 rounded-full">
+                            </span>
                             <span class="flex flex-col justify-center">
-                                <span class="text-xl">{{ Auth::user()->name }}</span>
+                                <span class="flex items-center space-x-2">
+                                    <span class="text-lg">{{ Auth::user()->name }}</span>
+                                </span>
                                 <span class="text-[#179fdf]">You're logged in!</span>
                             </span>
                         </div>
                     </span>
-                    <span class="cursor-pointer">
-                        <span href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                         document.getElementById('logout-form').submit();">
-                            <img src="{{ asset('images/logout.svg') }}" alt="logout" class="w-6 h-6">
-                        </span>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
-                        </form>
-                    </span>
+                    <div class="flex justify-end items-baseline relative">
+                        <span class="w-6 h-6  rounded-full flex items-center justify-center absolute left-1 top-1 right-1 text-[#f0831e] text-md font-bold">37</span>
+                        <img src="{{ URL::asset('/images/cart.svg') }}" alt="cart" class="w-12 h-10">
+                        <h1 class="text-lg">{{__('Cart')}}</h1>
+                    </div>
                 </li>
                 @endguest
             </ul>
+            <div class="w-full flex justify-between items-center px-8">
+                <a href="" class="text-[#ffffff] text-lg font-thin hover:text-gray-400">{{__('DASHBOARD')}}</a>
+                <span class="cursor-pointer">
+                    <span href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                             document.getElementById('logout-form').submit();">
+                        <img src="{{ asset('images/logout.svg') }}" alt="logout" class="w-6 h-6">
+                    </span>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </span>
+            </div>
         </div>
         <div class="container w-9/12 p-0">
-
         </div>
     </div>
 </body>
